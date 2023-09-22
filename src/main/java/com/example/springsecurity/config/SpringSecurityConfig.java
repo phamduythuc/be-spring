7691ade6bf8deb421fd08e91e593f6d868b8f2e4
ADMIN <songkhecole@gmail.com>
2023-09-22 09:02:33 +0700

    a

--- src/main/java/com/example/springsecurity/config/SpringSecurityConfig.java
+++ src/main/java/com/example/springsecurity/config/SpringSecurityConfig.java
@@ -1,28 +1,18 @@
 package com.example.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public static PasswordEncoder passwordEncoder() {
         return new  BCryptPasswordEncoder();
     }
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user1 = User.builder().username("user1").password(passwordEncoder().encode("user1")).roles("USER").build();
-        UserDetails user2 = User.builder().username("user2").password(passwordEncoder().encode("user2")).roles("ADMIN").build();
-        return new InMemoryUserDetailsManager(user1, user2);
-    }
+
 }
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,6 @@
+public class MainTest {
+    public static void main(String[] args) {
+        double a=9.45;
+        System.out.println((int)a);
+    }
+}
