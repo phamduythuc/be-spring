e8fd0e0333d00188e1fa21ae0284cbca5dc44a1f
ADMIN <songkhecole@gmail.com>
2023-10-09 09:04:41 +0700

    update MainTest.java

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,6 +1,40 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+
+@Component
 public class MainTest {
+    @Autowired
+    private UserDetailsService userDetailsService;
     public static void main(String[] args) {
-        double a=9.45;
-        System.out.println((int)a);
+        double v=3.14;
+        System.out.println((long)v);
     }
 }
+
+@Data
+@AllArgsConstructor
+class Person {
+    private int id;
+    private String name;
+    private int age;
+    private String address;
+
+}
+
+@Data
+class PersonDTO {
+    private int id;
+    private String name;
+    private int age;
+
+    public PersonDTO(Person person) {
+        this.id = person.getId();
+        this.name = person.getName();
+        this.age = person.getAge();
+    }
+}
+
