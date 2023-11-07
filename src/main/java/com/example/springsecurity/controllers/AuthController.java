f28a299d2d68c37e0e2e8913e1f77efd5177f8eb
thuc duy <thucphamduy182@gmail.com>
2023-11-07 09:20:44 +0700

    update code

--- src/main/java/com/example/springsecurity/controllers/AuthController.java
+++ src/main/java/com/example/springsecurity/controllers/AuthController.java
@@ -9,7 +9,7 @@ import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
